98c5669c3208a285be63ee0a3fdd4000d2809457
Kieran Harvey <kieran.harvey96@gmail.com>
2023-10-02 12:54:45 +0200

    #1 - Initial commit

---
+++ x-clone/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const supabase = createServerComponentClient(cookies);
+  const { data: posts } = await supabase.from("posts").select("*");
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Hola mundo!
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </main>
+  );
+}
